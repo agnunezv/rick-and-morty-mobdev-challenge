c8b319624a2898af2f3070e4f82b9d278863d369
Andrés G. Núñez Visbal <andriel.nunez@gmail.com>
2024-07-05 16:37:59 -0400

    Initial commit. 🏁

---
+++ src/main/java/org/example/rickandmortymobdevchallenge/RickAndMortyMobdevChallengeApplication.java
@@ -0,0 +1,13 @@
+package org.example.rickandmortymobdevchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickAndMortyMobdevChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RickAndMortyMobdevChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/rickandmortymobdevchallenge/RickAndMortyMobdevChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.rickandmortymobdevchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickAndMortyMobdevChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
