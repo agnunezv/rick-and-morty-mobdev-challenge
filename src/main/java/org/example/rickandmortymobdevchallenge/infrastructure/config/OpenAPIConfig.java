66c4caa13218e74ea78dad075399c7e365cfa088
Andrés G. Núñez Visbal <andriel.nunez@gmail.com>
2024-07-06 13:02:43 -0400

    - Added Swagger to application.

---
+++ src/main/java/org/example/rickandmortymobdevchallenge/infrastructure/config/OpenAPIConfig.java
@@ -0,0 +1,19 @@
+package org.example.rickandmortymobdevchallenge.infrastructure.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Tu API")
+                        .version("1.0.0")
+                        .description("Descripción de tu API"));
+    }
+}
